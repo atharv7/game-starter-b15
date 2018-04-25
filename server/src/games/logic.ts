0eee808dc67141d2f502ec95fcb41fcc17e0afa1
SaraRiach <saracar2@gmail.com>
2018-04-25 14:38:33 +0200

    added test

--- server/src/games/logic.ts
+++ server/src/games/logic.ts
@@ -1,8 +1,8 @@
-import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
-import { Board, Symbol, Row } from './entities'
+import { ValidatorConstraint } from 'class-validator'
+import { Board} from './entities'
 
 @ValidatorConstraint()
-export class IsBoard implements ValidatorConstraintInterface {
+export class IsBoard {
 
 //   validate(board: Board) {
 //     const symbols = [ '1', '2', '3', '4', '5', '6' ]
@@ -14,40 +14,47 @@ export class IsBoard implements ValidatorConstraintInterface {
 //   }
 // }
 
-export const isValidTransition = (playerSymbol: Symbol, from: Board, to: Board) => {
-  const changes = from
-    .map(
-      (row, rowIndex) => row.map((symbol, columnIndex) => ({
-        from: symbol,
-        to: to[rowIndex][columnIndex]
-      }))
-    )
-    .reduce((a,b) => a.concat(b))
-    .filter(change => change.from !== change.to)
+// export const isValidTransition = (playerSymbol: Symbol, from: Board, to: Board) => {
+//   const changes = from
+//     .map(
+//       (row, rowIndex) => row.map((symbol, columnIndex) => ({
+//         from: symbol,
+//         to: to[rowIndex][columnIndex]
+//       }))
+//     )
+//     .reduce((a,b) => a.concat(b))
+//     .filter(change => change.from !== change.to)
 
-  return changes.length === 1 &&
-    changes[0].to === playerSymbol &&
-    changes[0].from === null
+//   return changes.length === 1 &&
+//     changes[0].to === playerSymbol &&
+//     changes[0].from === null
+// 
 }
 
-export const calculateWinner = (board: Board): Symbol | null =>
-  board
-    .concat(
-      // vertical winner
-      [0, 1, 2].map(n => board.map(row => row[n])) as Row[]
-    )
-    .concat(
-      [
-        // diagonal winner ltr
-        [0, 1, 2].map(n => board[n][n]),
-        // diagonal winner rtl
-        [0, 1, 2].map(n => board[2-n][n])
-      ] as Row[]
-    )
-    .filter(row => row[0] && row.every(symbol => symbol === row[0]))
-    .map(row => row[0])[0] || null
+export const calculateWinner = (board: Board): [number,number] =>{
+  // board
+  //   .concat(
+  //     // vertical winner
+  //     [0, 1, 2].map(n => board.map(row => row[n])) as Row[]
+  //   )
+  //   .concat(
+  //     [
+  //       // diagonal winner ltr
+  //       [0, 1, 2].map(n => board[n][n]),
+  //       // diagonal winner rtl
+  //       [0, 1, 2].map(n => board[2-n][n])
+  //     ] as Row[]
+  //   )
+  //   .filter(row => row[0] && row.every(symbol => symbol === row[0]))
+  //   .map(row => row[0])[0] || null
+  if(board[0]>board[1]) {
+    return [1,0]
+  } else {
+      if(board[0]===board[1]){
+        return [1,1]
+      } else {
+    return [0,1]
+  }
+  }
+}
 
-export const finished = (board: Board): boolean =>
-  board
-    .reduce((a,b) => a.concat(b) as Row)
-    .every(symbol => symbol !== null)
